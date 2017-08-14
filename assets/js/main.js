bf9aad908e4784896529a6a761919dae6b281bb7
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-08-14 17:45:59 -0500

    eliminar fotos con un boton

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+// var pictures = ["assets/img/img1.jpg","assets/img/img2.jpg", "assets/img/img3.jpg", "assets/img/img4.jpg"];
+// document.getElementsByClassName("container_images");
+// var div1 = document.createElement("div");
+// document.getElementsByClassName("imagenes").appendChild(div1);
+// for(var i=1; i< pictures.length ; i++){
+// 	var imagenes =document.getElementsByClassName("img"+i);
+// 	var imagen1 = pictures[i];
+// 	imagenes.style.background=('')
+// }
+
+function myFunction() {
+    var list = document.getElementById("myList");
+    list.removeChild(list.childNodes[0]);
+}
