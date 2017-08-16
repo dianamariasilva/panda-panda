8353d4a39dea4fc18fda51830bbc24d09ca6a951
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-08-15 21:59:00 -0500

    pag funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,27 @@
 // 	imagenes.style.background=('')
 // }
 
-function myFunction() {
+function elimImg() {
     var list = document.getElementById("myList");
     list.removeChild(list.childNodes[0]);
 }
+
+var paragraph1 = document.createElement('p');
+var paragraph2 = document.createElement('p');
+var div1 = document.createElement('DIV');
+var div2 = document.createElement('DIV');
+var parrafos = document.getElementById("parrafos");
+var text = document.createTextNode('Lorem ipsum dolor sit amet, consectetur   adipisicing elit. Non dignissimos veniam, nostrum repellendus facere iusto ipsum, aliquam! Rem libero enim, asperiores quasi, ducimus, hic nulla nesciunt, culpa sed perspiciatis possimus?');
+var text2 = document.createTextNode('Lorem ipsum dolor sit amet, consectetur   adipisicing elit. Non dignissimos veniam, nostrum repellendus facere iusto ipsum, aliquam! Rem libero enim, asperiores quasi, ducimus, hic nulla nesciunt, culpa sed perspiciatis possimus?');
+
+//document.getElementsByTagName("p")[0].setAttribute("class", "parr1");
+paragraph1.appendChild(text);
+div1.appendChild(paragraph1);
+parrafos.appendChild(div1);
+
+paragraph2.appendChild(text2);
+div2.appendChild(paragraph2);
+parrafos.appendChild(div2);
+
+
+
